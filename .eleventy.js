3bd6d7657eb64d7de5fd61d1bed096c9515977ab
cauangabriel774-cell <cauangabriel774@gmail.com>
2026-04-26 15:32:40 -0300

    Apenas arquivos que não são reviews
    Removed unnecessary passthrough copy configurations for years 2021, 2023, 2025, and 2026.

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,13 @@
 module.exports = function(eleventyConfig) {
-  
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("Imagens");
   eleventyConfig.addPassthroughCopy("ArtistaFotos");
-  eleventyConfig.addPassthroughCopy("2021");
-  eleventyConfig.addPassthroughCopy("2023");
-  eleventyConfig.addPassthroughCopy("2025");
-  eleventyConfig.addPassthroughCopy("2026");
 
   return {
     dir: {
       input: ".",
-      output: "_site"
+      output: "_site",
+      includes: "_includes" // Isso garante que ele ache seus layouts
     }
   };
 };
