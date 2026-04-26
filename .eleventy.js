9855dbdaa5e379ae0f3be37e8d33ecea9b139cce
cauangabriel774-cell <cauangabriel774@gmail.com>
2026-04-26 18:40:57 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,22 @@
 module.exports = function(eleventyConfig) {
 
-  // Arquivos estáticos
+  // Copiar arquivos estáticos
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("Imagens");
   eleventyConfig.addPassthroughCopy("ArtistaFotos");
 
-  // Filtro de data
+  // Filtro de data pt-BR
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     if (!dateObj) return "";
     return new Date(dateObj).toLocaleDateString("pt-BR");
   });
 
-  // COLEÇÃO CORRETA (ordenada por data)
-  eleventyConfig.addCollection("buscaDiscos", function(collectionApi) {
-  return collectionApi.getAll()
-    .filter(item => item.data.album)
-    .sort((a, b) => new Date(b.date) - new Date(a.date))
-    .map(item => {
-      return {
-        title: item.data.album,
-        artist: item.data.artista,
-        url: item.url,
-        img: item.data.imagem
-      };
-    });
-});
+  // COLEÇÃO CORRETA (sem bug)
+  eleventyConfig.addCollection("meusDiscos", function(collectionApi) {
+    return collectionApi.getAll()
+      .filter(item => item.data.album)
+      .sort((a, b) => new Date(b.date) - new Date(a.date));
+  });
 
   return {
     dir: {
