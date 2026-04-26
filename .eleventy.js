5fefa7be7c5100d7ba2aed2fd6370e937443211a
cauangabriel774-cell <cauangabriel774@gmail.com>
2026-04-26 15:36:25 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,17 @@
 module.exports = function(eleventyConfig) {
+  
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("Imagens");
   eleventyConfig.addPassthroughCopy("ArtistaFotos");
+  eleventyConfig.addPassthroughCopy("2021");
+  eleventyConfig.addPassthroughCopy("2023");
+  eleventyConfig.addPassthroughCopy("2025");
+  eleventyConfig.addPassthroughCopy("2026");
 
   return {
     dir: {
       input: ".",
-      output: "_site",
-      includes: "_includes" // Isso garante que ele ache seus layouts
+      output: "_site"
     }
   };
 };
