b014cc07ddb91ceeae681d7d651d87fb778ba584
cauangabriel774-cell <cauangabriel774@gmail.com>
2026-04-26 09:23:26 -0300

    Configure passthrough copy for Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,1 +1,17 @@
+module.exports = function(eleventyConfig) {
+  
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("Imagens");
+  eleventyConfig.addPassthroughCopy("ArtistaFotos");
+  eleventyConfig.addPassthroughCopy("2021");
+  eleventyConfig.addPassthroughCopy("2023");
+  eleventyConfig.addPassthroughCopy("2025");
+  eleventyConfig.addPassthroughCopy("2026");
 
+  return {
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
+};
