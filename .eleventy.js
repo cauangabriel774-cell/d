e80517cca50bf0fc504489c7e874d1ab9c7508b2
cauangabriel774-cell <cauangabriel774@gmail.com>
2026-04-27 09:06:16 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -9,18 +9,22 @@ module.exports = function(eleventyConfig) {
     return new Date(dateObj).toLocaleDateString("pt-BR");
   });
 
-  eleventyConfig.addCollection("meusDiscos", function(collectionApi) {
-    return collectionApi.getAll()
-      .filter(item => item.data.album)
-      .sort((a, b) => new Date(b.date) - new Date(a.date));
+  eleventyConfig.addCollection("artistas", function(collectionApi) {
+  const discos = collectionApi.getAll().filter(i => i.data.artista);
+
+  const map = {};
+
+  discos.forEach(disco => {
+    const nome = disco.data.artista;
+
+    if (!map[nome]) {
+      map[nome] = {
+        nome,
+        slug: nome.toLowerCase().replace(/\s+/g, "-"),
+        discos: []
+      };
+    }
+
+    map[nome].discos.push(disco);
   });
 
-  return {
-    dir: {
-      input: ".",
-      output: "_site",
-      includes: "_includes"
-    }
-  };
-
-};
