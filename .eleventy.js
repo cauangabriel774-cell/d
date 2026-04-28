c3d5632fa2078f1ccd7ea5f18a2010c2b66f3ce5
cauangabriel774-cell <cauangabriel774@gmail.com>
2026-04-27 22:15:43 -0300

    Add 'meusDiscos' collection to Eleventy config
    Added a new collection for 'meusDiscos' that filters and sorts albums by date.

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,22 @@
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy("Imagens");
-  eleventyConfig.addPassthroughCopy("ArtistaFotos");
+  // ...
 
-  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
-    if (!dateObj) return "";
-    return new Date(dateObj).toLocaleDateString("pt-BR");
+  eleventyConfig.addCollection("meusDiscos", function(collectionApi) {
+    return collectionApi.getAll()
+      .filter(item => item.data.album)
+      .sort((a, b) => new Date(b.date) - new Date(a.date));
   });
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+      includes: "_includes"
+    }
+  };
+};
 
   // 🔥 COLEÇÃO ARTISTAS
   eleventyConfig.addCollection("artistas", function(collectionApi) {
